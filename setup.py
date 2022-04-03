2f7826ff1d8ee98ba01a1251548b0ac55873f2a7
Rodrigo Canales <rocanale@gmail.com>
2022-04-03 17:29:46 +0200

    Adding missing fire dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ import climl
 setup(
     name='climl',
     packages=['climl', 'climl.utils'],
-    install_requires=['pandas >= 1.4.2', 'scikit-learn >= 1.0.2', 'numpy >= 1.22.3'],
+    install_requires=['pandas >= 1.4.2',
+     'scikit-learn >= 1.0.2',
+      'numpy >= 1.22.3',
+      'fire >= 0.4.0',
+      ],
     python_requires='>=3.8.0',
     entry_points = {
         'console_scripts' : ['climl = climl.app:main']
