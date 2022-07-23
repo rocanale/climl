23a2464c124e10643bcf39d0ebbe91181924fde9
Rodrigo Canales <rocanale@gmail.com>
2022-07-23 23:56:32 +0200

    Updating the python version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         "numpy >= 1.22.3",
         "fire >= 0.4.0",
     ],
-    python_requires=">=3.8.0",
+    python_requires=">=3.10",
     entry_points={"console_scripts": ["climl = climl.app:main"]},
     version=climl.__version__,
     description="A test package",
