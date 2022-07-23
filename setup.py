8df03d70ff6262eab066ba3c13b568e82785a52e
Rodrigo Canales <rocanale@gmail.com>
2022-07-23 21:25:53 +0200

    Fixing setup

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@ import climl
 
 
 setup(
-    name='climl',
-    packages=['climl', 'climl.utils'],
-    install_requires=['pandas >= 1.4.2',
-     'scikit-learn >= 1.0.2',
-      'numpy >= 1.22.3',
-      'fire >= 0.4.0',
-      ],
-    python_requires='>=3.8.0',
-    entry_points = {
-        'console_scripts' : ['climl = climl.app:main']
-    },
+    name="climl",
+    packages=["climl", "climl.utils"],
+    install_requires=[
+        "pandas >= 1.4.2",
+        "scikit-learn >= 1.0.2",
+        "numpy >= 1.22.3",
+        "fire >= 0.4.0",
+    ],
+    python_requires=">=3.8.0",
+    entry_points={"console_scripts": ["climl = climl.app:app"]},
     version=climl.__version__,
-    description='A test package',
-    author='rocanale',
-    author_email='',
-    url=''
+    description="A test package",
+    author="rocanale",
+    author_email="",
+    url="",
 )
